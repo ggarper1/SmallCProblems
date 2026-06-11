08ff194f8a6e03afb5732b21c4aef0552bfa736a
ggarper1 <137006154+ggarper1@users.noreply.github.com>
2026-06-11 18:37:01 +0200

    refactor: stack rename

--- modules/stack/tests/stackTests.c
+++ modules/stack/tests/stackTests.c
@@ -7,13 +7,13 @@
 const int MAX = -10000000;
 const int MIN = 10000000;
 
-int randInt(int min, int max) {
+int rand_int(int min, int max) {
   int range = max - min + 1;
   return (rand() % range) + min;
 }
 
-void checkEmptyStack(Stack_t *stack) {
-  StackResult result = stackPeek(stack);
+void check_empty_stack(my_stack_t *stack) {
+  stack_result_t result = stackPeek(stack);
   assert(result.status == STACK_EMPTY && result.value == NULL);
 
   result = stackPop(stack);
@@ -24,18 +24,18 @@ void checkEmptyStack(Stack_t *stack) {
 
 void testBasicStack(int itemCount) {
 
-  Stack_t *stack = newStack(1);
+  my_stack_t *stack = newStack(1);
 
-  checkEmptyStack(stack);
+  check_empty_stack(stack);
 
   int *values = malloc(sizeof(int) * itemCount);
   for (int i = 0; i < itemCount; i++) {
-    values[i] = randInt(MIN, MAX);
+    values[i] = rand_int(MIN, MAX);
 
     STACK_STATUS status = stackPush(stack, &values[i]);
     assert(status == STACK_OK);
 
-    StackResult result = stackPeek(stack);
+    stack_result_t result = stackPeek(stack);
     assert(result.status != STACK_ERROR);
     assert((result.status == STACK_EMPTY && result.value == NULL && i == 0) ||
            (result.status == STACK_OK && *(int *)result.value == values[i]));
@@ -44,7 +44,7 @@ void testBasicStack(int itemCount) {
   }
 
   for (int i = itemCount - 1; i >= 0; i--) {
-    StackResult result = stackPeek(stack);
+    stack_result_t result = stackPeek(stack);
     assert(result.status != STACK_ERROR);
     assert((result.status == STACK_EMPTY && result.value == NULL && i == 0) ||
            (result.status == STACK_OK && *(int *)result.value == values[i]));
@@ -57,20 +57,20 @@ void testBasicStack(int itemCount) {
     assert(stackSize(stack) == i);
   }
 
-  checkEmptyStack(stack);
+  check_empty_stack(stack);
 
   stackDestroy(stack, NULL);
 
   free(values);
 }
 
-void testNulls() {
-  Stack_t *stack = newStack(1);
+void test_nulls() {
+  my_stack_t *stack = newStack(1);
   assert(stackPush(stack, NULL) == STACK_OK);
 
   assert(stackSize(stack) == 1);
 
-  StackResult result = stackPeek(stack);
+  stack_result_t result = stackPeek(stack);
   assert(result.status == STACK_OK && result.value == NULL);
 
   result = stackPop(stack);
@@ -84,14 +84,14 @@ void testNulls() {
   stackDestroy(stack, NULL);
 }
 
-void testRandomPushPops() {
-  Stack_t *stack = newStack(5);
+void test_random_push_pops() {
+  my_stack_t *stack = newStack(5);
 
   int *values = malloc(sizeof(int) * 100000);
 
   int size = 0;
   while (size < 5) {
-    values[size] = randInt(MIN, MAX);
+    values[size] = rand_int(MIN, MAX);
     assert(stackPush(stack, &values[size]) == STACK_OK);
     size++;
   }
@@ -99,14 +99,14 @@ void testRandomPushPops() {
   for (int i = 0; i < 100000; i++) {
     assert(stackSize(stack) == size);
 
-    StackResult result = stackPeek(stack);
+    stack_result_t result = stackPeek(stack);
 
     assert((size > 0 && result.status == STACK_OK &&
             (int *)result.value == &values[size - 1]) ||
            (result.status == STACK_EMPTY && result.value == NULL));
 
-    if (size == 0 || randInt(0, 1)) {
-      values[size] = randInt(MIN, MAX);
+    if (size == 0 || rand_int(0, 1)) {
+      values[size] = rand_int(MIN, MAX);
       assert(stackPush(stack, &values[size]) == STACK_OK);
       size++;
     } else {
@@ -122,20 +122,20 @@ void testRandomPushPops() {
   free(values);
 }
 
-void freeValue(void *value) { free(value); }
+void free_value(void *value) { free(value); }
 
-void testDestroyWithFunction() {
-  Stack_t *stack = newStack(5);
+void test_destroy_with_function() {
+  my_stack_t *stack = newStack(5);
   int *v = malloc(sizeof(int));
   stackPush(stack, v);
 
-  stackDestroy(stack, freeValue);
+  stackDestroy(stack, free_value);
 }
 
 int main(int argc, char *argv[]) {
-  testNulls();
-  testDestroyWithFunction();
-  testRandomPushPops();
+  test_nulls();
+  test_destroy_with_function();
+  test_random_push_pops();
 
   int baseItemCount = 100;
   for (int factor = 0; factor < 1000; factor++) {
