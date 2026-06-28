1c0bd1a5fe32773abbdd29b8e01ba527ce7b9858
ggarper1 <137006154+ggarper1@users.noreply.github.com>
2026-06-28 21:35:13 +0200

    refactor: bool stack

--- modules/bool_stack/include/bool_stack.h
+++ modules/bool_stack/include/bool_stack.h
@@ -1,11 +1,20 @@
 #include <stddef.h>
 
-#ifndef BOOLSTACK_H
-#define BOOLSTACK_H
+#ifndef BOOL_STACK_H
+#define BOOL_STACK_H
 
-typedef enum { BS_ERROR, BS_OK } BS_STATUS;
+typedef enum {
+  BOOL_STACK_ERROR,
+  BOOL_STACK_OK,
+  BOOL_STACK_EMPTY
+} BOOL_STACK_STATUS;
 
-typedef struct Stack BoolStack_t;
+typedef struct stack_result {
+  BOOL_STACK_STATUS status;
+  int value;
+} bool_stack_result_t;
+
+typedef struct stack bool_stack_t;
 
 // --- Public Function Prototypes ---
 
@@ -14,7 +23,7 @@ typedef struct Stack BoolStack_t;
  * @param capacity Initial number of items.
  * @return A pointer to the new Stack, or NULL on failure.
  */
-BoolStack_t *newBoolStack(size_t capacity);
+bool_stack_t *new_bool_stack(size_t capacity);
 
 /**
  * Returns a Stack's length (number of values in it).
@@ -22,40 +31,34 @@ BoolStack_t *newBoolStack(size_t capacity);
  * @return the number of items in it.
  */
 
-int bsLength(BoolStack_t *stack);
+int bool_stack_size(bool_stack_t *stack);
 /**
  * Peeks the front element of a Stack.
  * @param stack The stack.
  * @return A pointer to the element, NULL if the stack is empty.
  */
-int bsPeek(BoolStack_t *stack);
+bool_stack_result_t bool_stack_peek(bool_stack_t *stack);
 
 /**
  * Adds a value in a Stack.
  * Pointers to the item are stored (caller manages memory).
  * @param stack The Stack.
  * @param value The value to add.
- * @return the status of the operation (BS_OK or BS_ERROR).
+ * @return the status of the operation (BOOL_STACK_OK or BS_ERROR).
  */
-BS_STATUS bsPush(BoolStack_t *stack, int value);
+BOOL_STACK_STATUS bool_stack_push(bool_stack_t *stack, int value);
 
 /**
  * Removes the front element of a Stack.
  * @param stack The Stack.
  * @return NULL if the Stack is empty, the pointer to the value if it
  */
-int bsPop(BoolStack_t *stack);
+bool_stack_result_t bool_stack_pop(bool_stack_t *stack);
 
 /**
  * Destroys the Stack and frees all allocated memory.
  * @param stack The Stack to destroy.
  */
-void bsDestroy(BoolStack_t *stack);
-
-/**
- * Prints the Stack.
- * @param stack The Stack to print.
- */
-void printBoolStack(BoolStack_t *stack);
+void bool_stack_destroy(bool_stack_t *stack);
 
 #endif
